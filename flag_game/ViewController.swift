f0fb697f62317c267dda488a0f324a17b075a23d
Vaughn Antonyan <vaughnanton@gmail.com>
2017-10-03 13:40:21 -0700

    Add buttion functionality and score tracking

--- flag_game/ViewController.swift
+++ flag_game/ViewController.swift
@@ -31,10 +31,10 @@ class ViewController: UIViewController {
     button2.layer.borderColor = UIColor.lightGray.cgColor
     button3.layer.borderColor = UIColor.lightGray.cgColor
         
-        askQuestion()
+        askQuestion(action: nil)
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction!) {
         countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
         
         button1.setImage(UIImage(named: countries[0]), for: .normal)
@@ -45,7 +45,23 @@ class ViewController: UIViewController {
 
         title = countries[correctAnswer].uppercased()
     }
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
